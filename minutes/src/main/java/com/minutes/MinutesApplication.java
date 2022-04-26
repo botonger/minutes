3db72041f0b59dee4bdad2f5952df093f2f8dee4
botonger <shinecheyenne@gail.com>
2022-04-26 23:34:45 +0900

    modify: added auditing-annotation

--- minutes/src/main/java/com/minutes/MinutesApplication.java
+++ minutes/src/main/java/com/minutes/MinutesApplication.java
@@ -2,9 +2,11 @@ package com.minutes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoAuditing
 @EnableMongoRepositories
 public class MinutesApplication {
 	public static void main(String[] args) {
