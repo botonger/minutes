cfc4bb805075b789f85acd449d0954de91ed79bf
botonger <shinecheyenne@gail.com>
2022-04-26 23:38:25 +0900

    feat: model

--- minutes/src/main/java/com/minutes/model/Member.java
+++ minutes/src/main/java/com/minutes/model/Member.java
@@ -1,24 +1,33 @@
 package com.minutes.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.*;
+
 
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
+@ToString
+@Encrypted(keyId = "chocolate", algorithm = "AEAD_AES_256_CBC_HMAC_SHA_512-Random")
 @Document(collection = "members")
 public class Member {
     @Id
+    private ObjectId id;
+
+    @Indexed(unique = true)
     private String email;
 
     private String name;
+
+    @Encrypted
     private String password;
-    private String role;
+
+//    @DocumentReference(lazy = true)
+//    private Set<Room> rooms;
 
 }
